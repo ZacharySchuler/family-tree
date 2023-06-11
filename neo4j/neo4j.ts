a804f004b6ee97f706c431e500761ae1d680dfe7
Zachary Schuler <zachschuler@zachs-mbp.myfiosgateway.com>
2023-06-11 00:38:02 -0400

    reading for neo4j. Need to add better types

--- neo4j/neo4j.ts
+++ neo4j/neo4j.ts
@@ -1,4 +1,5 @@
 import neo4j, { Node, Relationship, Integer } from 'neo4j-driver'
+import { list } from 'postcss';
 
 
 interface PersonProperties {
@@ -37,18 +38,18 @@ export async function main(): Promise<Graph> {
   
     try {
       // Execute a Cypher statement in a Read Transaction
-      const nodesRes = await session.executeRead(tx => tx.run<PersonNode>(`MATCH (n) RETURN n;`))
-      const relationRes = await session.executeRead(tx => tx.run<Relation>(`MATCH (n) OPTIONAL MATCH (n)-[r]-() RETURN r;`))
+      const nodesRes = await session.executeRead(tx => tx.run(`MATCH (n) RETURN n;`))
+      const relationRes = await session.executeRead(tx => tx.run(`MATCH (n) OPTIONAL MATCH (n)-[r]-() RETURN r;`))
 
     
-      const persons = nodesRes.records.map(node => node.get('n'))
+      const persons = await nodesRes.records.map(node => node.get('n'))
       
 
-      const relationships = relationRes.records.map(relation => relation.get('r'))
+      const rawRelationships = await relationRes.records.map(relation => relation.get('r'))
 
       const graph:Graph = {
         nodeList: persons,
-        relationshipList: relationships
+        relationshipList: rawRelationships
       }
 
       return graph;
@@ -60,6 +61,69 @@ export async function main(): Promise<Graph> {
     finally {
       // Close the Session
       await session.close()
+    }
+    
+  }
+
+export async function formatGraphData(){
+
+    const graph = await main()
+    const nodes = graph.nodeList
+    const rawRelationship = graph.relationshipList
+
+    const relationships = await rawRelationship.map(relationship =>{
+        const start = findRelationshipStart(relationship, nodes);
+        const end = findRelationshipEnd(relationship, nodes);
+        return({
+            type: relationship.type,
+            start:start,
+            end: end
+        })
+    
+    })
+
+
+        return {
+            nodeList: nodes,
+            relationships: relationships
+        }
+}
+
+
+
+
+function findRelationshipStart(relation: any, listOfNodes: any[]): any{
+
+    let name
+
+    listOfNodes.forEach(node => {
+      if(isIdentityEqual(relation.start, node.identity)){
+      name = node.properties.name}
+      else{
+        "Shit"
+      }
+      
+    });
+    return name;
+  }
+
+  function findRelationshipEnd(relation: any, listOfNodes: any[]): any{
+
+    let name 
+    listOfNodes.forEach(node => {
+      if(isIdentityEqual(relation.end, node.identity)){
+      name = node.properties.name}
+      else{
+        return "shit"
+      }
+    });
+
+    return name
+  }
+
+  function isIdentityEqual(relationshipId,nodeId){
+    if(relationshipId.low == nodeId.low && relationshipId.high == nodeId.high){
+      return true
     }
     
   }
--- src/server/api/routers/neo4j/readFromNeo4j.ts
+++ src/server/api/routers/neo4j/readFromNeo4j.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import {main, Graph} from "neo4j/neo4j"
+import {main, Graph, formatGraphData} from "neo4j/neo4j"
 
 
 
@@ -8,7 +8,7 @@ import {main, Graph} from "neo4j/neo4j"
 
 export const graphRouter = createTRPCRouter({
   getAllData: publicProcedure
-    .query(async (data) => { const graph = await main();
+    .query(async (data) => { const graph = await formatGraphData().then(data => data);
     return graph})
 });
 
